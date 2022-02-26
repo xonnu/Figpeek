fb98ed59dc2cf7284cb74b8ca5614b8db2095705
Justin Pascual <chrono.social@outlook.com>
2022-02-27 01:50:53 +0800

    💄 Change max length of title input

--- src/App.svelte
+++ src/App.svelte
@@ -35,12 +35,12 @@
 
   <div class="flex flex-col gap-2">
     <label class="text-sm" for="title">Project Title</label>
-    <input maxlength="25" class="w-[300px] border rounded-md py-2 px-4 text-sm" type="text" id="title" bind:value={title} on:change={capture}>
+    <input maxlength="40" class="w-[300px] border rounded-md py-2 px-4 text-sm" type="text" id="title" bind:value={title} on:change={capture}>
   </div>
 
   <div class="flex flex-col gap-2">
     <label class="text-sm" for="description">Project description</label>
-    <input class="w-[300px] border rounded-md py-2 px-4 text-sm" type="text" id="description" bind:value={(description)} on:change={capture}>
+    <input maxlength="50" class="w-[300px] border rounded-md py-2 px-4 text-sm" type="text" id="description" bind:value={(description)} on:change={capture}>
   </div>
   
   <a id="save" href="#a" class="bg-black text-white px-8 py-2 rounded-md w-fit text-sm" on:click={capture}>Save</a>
@@ -48,8 +48,8 @@
 
 <div class="w-full bg-white flex items-center justify-center pointer-events-none select-none pb-24 pt-10">
   <div id="capture" class="rounded-xl overflow-hidden px-[100px] flex flex-col gap-4 items-start justify-center w-[1240px] h-[640px] transform text-white" style:background-color={color}>
-    <h2 class="block font-bold text-7xl whitespace-normal text-left ml-24 w-[700px] leading-normal" on:change={capture}>{title || 'Figma Thumbnail Generator ✨'}</h2>
-    <p class="block mt-7 text-4xl whitespace-normal w-full text-left ml-24">{description || 'Welcome to Figma Thumbnail Generator'}</p>
+    <h2 class="block font-bold text-7xl whitespace-normal text-left ml-24 w-[700px] leading-[90px]" on:change={capture}>{title || 'Figma Thumbnail Generator ✨'}</h2>
+    <p class="block mt-7 text-4xl whitespace-normal w-full text-left ml-24 leading-[60px]">{description || 'Welcome to Figma Thumbnail Generator'}</p>
   </div>
 </div>
 
