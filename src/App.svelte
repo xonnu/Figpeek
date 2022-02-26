43e2049426a688775b52f6489038a1b04f52ef4b
Justin Pascual <chrono.social@outlook.com>
2022-02-27 02:24:14 +0800

    📱 remove mobile access

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,6 @@
   let description = 'Welcome to Figma Thumbnail Generator'
   let color = '#000'
 
-
   async function capture() {
     await tick();    
 
@@ -25,6 +24,11 @@
 
   capture()
 </script>
+
+<div class="p-10 container mx-auto fixed top-0 left-0 flex flex-col items-center justify-center lg:hidden bg-black w-full h-screen z-50">
+  <h2 class="font-bold text-2xl text-white">Oops, this website is not available in mobile view.</h2>
+  <p class="mt-4 text-sm text-[#b1b1b1]">Well, if you want to enable it in mobile, check out my repository and help me 🙈</p>
+</div>
 
 <main class="main flex flex-col gap-4">
  
@@ -46,7 +50,7 @@
   <a id="save" href="#a" class="bg-black text-white px-8 py-2 rounded-md w-fit text-sm" on:click={capture}>Save</a>
 </main>
 
-<div class="w-full bg-white flex items-center justify-center pointer-events-none select-none pb-24 pt-10">
+<div class="hidden w-full bg-white lg:flex items-center justify-center pointer-events-none select-none pb-24 pt-10">
   <div id="capture" class="rounded-xl overflow-hidden px-[100px] flex flex-col gap-4 items-start justify-center w-[1240px] h-[640px] transform text-white" style:background-color={color}>
     <h2 class="block font-bold text-7xl whitespace-normal text-left ml-24 w-[700px] leading-[90px]" on:change={capture}>{title || 'Figma Thumbnail Generator ✨'}</h2>
     <p class="block mt-7 text-4xl whitespace-normal w-full text-left ml-24 leading-[60px]">{description || 'Welcome to Figma Thumbnail Generator'}</p>
